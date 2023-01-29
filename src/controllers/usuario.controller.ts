c8bbee6186806607824fa17ec0b03ee90379852b
Marcos Teixeira <marcxstx@gmail.com>
2023-01-29 11:39:33 -0300

    Creating Routes and Adding Controllers

---
+++ src/controllers/usuario.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsuarioRepository } from 'src/repositories/usuario.repository';
+
+@Controller('/usuarios')
+export class UsuarioController {
+  private usuarioRepository = new UsuarioRepository();
+  @Post()
+  async criaUsuario(@Body() dadosDoUsuario) {
+    this.usuarioRepository.salvar(dadosDoUsuario);
+    // return { status: 'usuario criado!' };
+    return dadosDoUsuario;
+  }
+  @Get()
+  async listaUsuarios() {
+    return this.usuarioRepository.listar();
+  }
+}
